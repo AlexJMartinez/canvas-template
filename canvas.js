036303e77ea4db9a201c38a77d2c7f04d8419cfd
AlexJMartinez <alexandr3martin3z@gmail.com>
2021-09-01 10:38:07 -0700

    comment out recent test

--- canvas.js
+++ canvas.js
@@ -9,23 +9,23 @@ function draw() {
       var c = canvas.getContext('2d');
 
       
-     c.beginPath();
-     c.quadraticCurveTo(50, 50 , 100, 75);
-     c.quadraticCurveTo(25, 100 , 65, 75);
-     c.closePath();   
-     c.stroke();
+    //  c.beginPath();
+    //  c.quadraticCurveTo(50, 50 , 100, 75);
+    //  c.quadraticCurveTo(25, 100 , 65, 75);
+    //  c.closePath();   
+    //  c.stroke();
 
-     c.beginPath();
-     c.quadraticCurveTo(10, 50 , 100, 75);
-     c.quadraticCurveTo(25, 100 , 65, 75);
-     c.closePath();
-     c.strokeStyle = "red"   
-     c.stroke();
+    //  c.beginPath();
+    //  c.quadraticCurveTo(10, 50 , 100, 75);
+    //  c.quadraticCurveTo(25, 100 , 65, 75);
+    //  c.closePath();
+    //  c.strokeStyle = "red"   
+    //  c.stroke();
 
-     c.beginPath();
-     c.quadraticCurveTo(10, 50 , 90, 75);
-     c.quadraticCurveTo(25, 125 , 65, 75);
-     c.closePath();   
-     c.fill();
+    //  c.beginPath();
+    //  c.quadraticCurveTo(10, 50 , 90, 75);
+    //  c.quadraticCurveTo(25, 125 , 65, 75);
+    //  c.closePath();   
+    //  c.fill();
     }
   }
