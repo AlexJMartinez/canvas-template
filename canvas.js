81e7ba64b8a639a0b8019ea5db67698abad1a124
AlexJMartinez <alexandr3martin3z@gmail.com>
2021-09-01 23:43:30 -0700

    got things to start looping

--- canvas.js
+++ canvas.js
@@ -8,24 +8,42 @@ function draw() {
     if (canvas.getContext) {
       var c = canvas.getContext('2d');
 
-      
-    //  c.beginPath();
-    //  c.quadraticCurveTo(50, 50 , 100, 75);
-    //  c.quadraticCurveTo(25, 100 , 65, 75);
-    //  c.closePath();   
-    //  c.stroke();
+    // for (let i = 0; i < 10; i++) {
 
-    //  c.beginPath();
-    //  c.quadraticCurveTo(10, 50 , 100, 75);
-    //  c.quadraticCurveTo(25, 100 , 65, 75);
-    //  c.closePath();
-    //  c.strokeStyle = "red"   
-    //  c.stroke();
+    //   let x = Math.random() * window.innerWidth;
+    //   let y = Math.random() * window.innerHeight;
 
-    //  c.beginPath();
-    //  c.quadraticCurveTo(10, 50 , 90, 75);
-    //  c.quadraticCurveTo(25, 125 , 65, 75);
-    //  c.closePath();   
-    //  c.fill();
+    //   c.beginPath();
+    //   c.arc(x, y, 100, Math.PI / 180 * Math.random(), Math.PI / 180 * Math.random(), false);
+    //   c.closePath();
+    //   // c.fillStyle='cyan';
+    //   // c.fill();
+    //   c.strokeStyle = "red"
+    //   c.stroke();
+    //   }
+
+    for (let i = 0; i < 50; i++) {
+
+      let x = Math.random() * window.innerWidth;
+      let y = Math.random() * window.innerHeight;
+
+      c.beginPath();
+      c.quadraticCurveTo(x, y, 50, x);
+      c.quadraticCurveTo(x, 100, x, 20);
+      c.quadraticCurveTo(x, 80, x, x);
+      // c.closePath();
+      // c.fillStyle='black';
+      // c.fill();
+      c.strokeStyle = 'black'
+      c.stroke();
+
+      }  
+     
     }
   }
+
+  window.setInterval(draw, 240)
+
+ 
+
+  
