770b9ae16fca1bd16ab4fef22fd636edf9f91628
AlexJMartinez <alexandr3martin3z@gmail.com>
2021-09-11 20:45:28 -0700

    deleted c.arc

--- canvas.js
+++ canvas.js
@@ -1,44 +1,28 @@
 
 function draw() {
-    var canvas = document.getElementById('canvas');
+    let canvas = document.getElementById('canvas');
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
     if (canvas.getContext) {
-      var c = canvas.getContext('2d');
-
-    // for (let i = 0; i < 10; i++) {
-
-    //   let x = Math.random() * window.innerWidth;
-    //   let y = Math.random() * window.innerHeight;
-
-    //   c.beginPath();
-    //   c.arc(x, y, 100, Math.PI / 180 * Math.random(), Math.PI / 180 * Math.random(), false);
-    //   c.closePath();
-    //   // c.fillStyle='cyan';
-    //   // c.fill();
-    //   c.strokeStyle = "red"
-    //   c.stroke();
-    //   }
+      let c = canvas.getContext('2d');
 
     for (let i = 0; i < 50; i++) {
 
       let x = Math.random() * window.innerWidth;
       let y = Math.random() * window.innerHeight;
 
+
       c.beginPath();
       c.quadraticCurveTo(x, y, 50, x);
       c.quadraticCurveTo(x, 100, x, 20);
       c.quadraticCurveTo(x, 80, x, x);
-      // c.closePath();
-      // c.fillStyle='black';
-      // c.fill();
+      
       c.strokeStyle = 'black'
       c.stroke();
 
       }  
-     
     }
   }
 
